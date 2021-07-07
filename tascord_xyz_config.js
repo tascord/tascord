82c6e287811a10b521d685b8717a8a61e0025dd8
Tascord <35181375+tascord@users.noreply.github.com>
2021-07-07 20:32:39 +1000

    Create tascord_xyz_config.js

---
+++ tascord_xyz_config.js
@@ -0,0 +1,21 @@
+const profile_data = {
+    title: 'tascord',
+    subtitle: 'programmer • video game enthusiast • woeful humor enjoyer',
+    bio: 'I do nothing but sit around making woefully written but good locking things in questionable programming languages.',
+    pronouns: 'they/them/theirs/themselves',
+    interests: 'programming, music, video games, big words, opulence, tea, energy drinks, pretty things, soft things, warm things',
+    links: {
+        instagram: 'tascord',
+        twitter: 'instagram',
+        discord: 'buzz',
+        pronouns: 'tascord'
+    },
+    images: {
+        // background: 'https://source.unsplash.com/1920x1080/?aesthetic',
+        background: 'https://images.unsplash.com/photo-1498736297812-3a08021f206f?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=1080&ixid=MnwxfDB8MXxyYW5kb218MHx8YWVzdGhldGljfHx8fHx8MTYyNTY1MDIxOQ&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1920',
+        background_offset: 'center center',
+        avatar: 'https://avatars.githubusercontent.com/u/35181375?v=4',
+        avatar_offset: '-25px center',
+        avatar_size: '150%'
+    }
+}
