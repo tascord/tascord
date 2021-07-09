209339d72b1f6ae7afcba3f5f5e17028c7cf74b8
Tascord <35181375+tascord@users.noreply.github.com>
2021-07-09 12:55:04 +1000

    Update tascord_xyz_config.js

--- tascord_xyz_config.js
+++ tascord_xyz_config.js
@@ -1,21 +1,20 @@
 const profile_data = {
-    title: 'tascord',
-    subtitle: 'programmer • video game enthusiast • woeful humor enjoyer',
-    bio: 'I do nothing but sit around making woefully written but good locking things in questionable programming languages.',
-    pronouns: 'they/them/theirs/themselves',
-    interests: 'programming, music, video games, big words, opulence, tea, energy drinks, pretty things, soft things, warm things',
-    links: {
-        instagram: 'tascord',
-        twitter: 'instagram',
-        discord: 'buzz',
-        pronouns: 'tascord'
+    "title": "tascord",
+    "subtitle": "programmer • video game enthusiast • woeful humor enjoyer",
+    "bio": "I do nothing but sit around making woefully written but good looking things in questionable programming languages.",
+    "pronouns": "they/them/theirs/themselves",
+    "interests": "programming, music, video games, big words, opulence, tea, energy drinks, pretty things, soft things, warm things",
+    "links": {
+        "instagram": "tascord",
+        "twitter": "tascord",
+        "discord": "buzz",
+        "pronouns": "tascord"
     },
-    images: {
-        // background: 'https://source.unsplash.com/1920x1080/?aesthetic',
-        background: 'https://images.unsplash.com/photo-1498736297812-3a08021f206f?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=1080&ixid=MnwxfDB8MXxyYW5kb218MHx8YWVzdGhldGljfHx8fHx8MTYyNTY1MDIxOQ&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1920',
-        background_offset: 'center center',
-        avatar: 'https://avatars.githubusercontent.com/u/35181375?v=4',
-        avatar_offset: '-25px center',
-        avatar_size: '150%'
+    "images": {
+        "background": "https://images.unsplash.com/photo-1566903218203-3524b6e427c5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+        "background_offset": "center",
+        "avatar": "https://avatars.githubusercontent.com/u/35181375?v=4",
+        "avatar_offset": "33% 43%",
+        "avatar_size": "154%"
     }
 }
